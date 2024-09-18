f7f1741376e48e3fbfee8855c7e98dfeeb82ca90
Oscar Alonso <oscar.alonso@posibillian.tech>
2024-09-18 15:54:14 -0400

    Importación y Exportación de módulos

--- 02-bases/src/app.js
+++ 02-bases/src/app.js
@@ -1,1 +1,5 @@
+// Importamos el módulo que exporta el template
+const templateExports = require('./js-foundation/01-template');
+
 console.log('Hello World!');
+console.log(templateExports.emailTemplate);
---
+++ 02-bases/src/js-foundation/01-template.js
@@ -0,0 +1,9 @@
+const emailTemplate = `
+	<h1>Hi, {{name}}</h1>
+	<p>Your email is: {{email}}</p>
+`;
+ 
+// Exportamos la plantilla
+module.exports = {
+	emailTemplate,
+};
