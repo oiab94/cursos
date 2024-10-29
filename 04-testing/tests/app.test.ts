0159c8308958abf13cabfce657be8169e505e32a
Oscar Alonso <oscar.alonso@posibillian.tech>
2024-10-29 16:30:59 -0300

    Prueba de los test

---
+++ 04-testing/tests/app.test.ts
@@ -0,0 +1,16 @@
+import {expect, describe, test} from "@jest/globals";
+
+describe("Test in app file", () => { 
+	
+	test("should be true", () => {
+		// 1. Arrange
+		const num1 = 1;
+		const num2 = 2;
+		
+		// 2. Act
+		const result = num1 + num2;
+		
+		// 3. Assert
+		expect(result).toBe(4);
+	});
+})
